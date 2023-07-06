349cf6c58fc2f7d800af835978b77f50bdd200f3
Yordy Carmona <yordy.carmona8@gmail.com>
2023-07-05 21:06:36 -0400

    Curso Javascript - ES6 avance 2

--- 01-Certificado-Algoritmos Y Estructra de Datos-JavaScript/02 - ES6/ES6.js
+++ 01-Certificado-Algoritmos Y Estructra de Datos-JavaScript/02 - ES6/ES6.js
@@ -74,17 +74,126 @@ const { today, tomorrow } = HIGH_TEMPERATURES;
 // ! 10 - Usa sintaxis de destructuracion para asignar variables desde objetos
 const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
 
-// ! 11 -
-// ! 12 -
-// ! 13 -
-// ! 14 -
-// ! 15 -
-// ! 16 -
-// ! 17 -
-// ! 18 -
-// ! 19 -
-// ! 20 -
-// ! 21 -
+// ! 11 - Usa sintaxis de desestructuración para asignar variables desde objetos anidados
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 },
+};
+
+const {
+  today: { low: lowToday, high: highToday2 },
+} = LOCAL_FORECAST;
+
+// ! 12 - Usa sintaxis de desestructuración para asignar variables desde arreglos
+
+let a = 8,
+  b = 6;
+[a, b] = [b, a];
+
+// ! 13 - Desestructuración vía elementos rest
+
+function removeFirstTwo(list) {
+  const [, , ...shorterList] = list;
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+// ! 14 - Utiliza sintaxis de desestructuración para pasar un objeto como parámetro de función
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85,
+};
+
+const half = ({ max, min }) => (max + min) / 2.0;
+
+// ! 15 - Crea cadenas usando plantillas literales
+
+const result = {
+  success: ['max-length', 'no-amd', 'prefer-arrow-functions'],
+  failure: ['no-var', 'var-on-top', 'linebreak'],
+  skipped: ['no-extra-semi', 'no-dup-keys'],
+};
+function makeList(arr) {
+  const failureItems = [];
+  for (const itemsLi of arr) {
+    const output = `<li class="text-warning">${itemsLi}</li>`;
+    failureItems.push(output);
+  }
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+// ! 16 - Escribe declaraciones concisas de objecto literales usando la abreviatura de propiedad de objeto
+
+const createPerson = (name, age, gender) => {
+  return {
+    name,
+    age,
+    gender,
+  };
+};
+
+// ! 17 - Escribe funciones breves y declarativas con ES6
+
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  },
+};
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+// ! 18 - Usa sintaxis de clases para definir una función constructora
+
+class Vegetable {
+  constructor(_name) {
+    this.name = _name;
+  }
+}
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Debería mostrar 'carrot'
+
+// ! 19 - Utiliza getters (accesores) y setters (mutadores) para controlar el acceso a un objeto
+
+class Thermostat {
+  constructor(fahrenheit) {
+    this._celsius = (5 / 9) * (fahrenheit - 32);
+  }
+
+  get temperature() {
+    return this._celsius;
+  }
+
+  set temperature(celsius) {
+    return (this._celsius = celsius);
+  }
+}
+
+const thermos = new Thermostat(76); // Ajuste en escala Farenheit
+let temp = thermos.temperature; // 24.44 en Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 en Celsius
+
+// ! 20 - Crea un módulo para scripts
+
+<>
+  <script src="index.js" type="module" />
+</>;
+
+// ! 21 - Utiliza la exportación para compartir un bloque de código
 // ! 22 -
 // ! 23 -
 // ! 24 -
